28f15ae7aa281a6f86f44d2eca87254a155750e2
ApoorvYdv <apoorv.ydv17@gmail.com>
2025-11-06 00:17:29 +0530

    Add developer router for configs

---
+++ src/insperion_api/routers/developer/config.py
@@ -0,0 +1,43 @@
+from typing import Annotated, Optional
+
+from fastapi import APIRouter, Depends
+
+from insperion_api.core.controllers.developer.config_controller import ConfigController
+from insperion_api.core.models.config import Config
+from insperion_api.core.schemas.developer.config import (
+    AddConfigRequest,
+    UpdateConfigRequest,
+)
+
+config_router = APIRouter(prefix="/v1/config", tags=["config"])
+
+
+@config_router.get("/{config_section}")
+async def get_configs(
+    controller: Annotated[ConfigController, Depends()],
+    config_section: Optional[str] = None,
+) -> list[Config]:
+    return await controller.get_configs(config_section)
+
+
+@config_router.post("")
+async def add_config(
+    request: AddConfigRequest, controller: Annotated[ConfigController, Depends()]
+) -> dict:
+    return await controller.add_config(request)
+
+
+@config_router.patch("/{config_id}")
+async def update_config(
+    config_id: int,
+    request: UpdateConfigRequest,
+    controller: Annotated[ConfigController, Depends()],
+) -> dict:
+    return await controller.update_config(config_id, request)
+
+
+@config_router.delete("/{config_id}")
+async def delete_config(
+    config_id: int, controller: Annotated[ConfigController, Depends()]
+) -> dict:
+    return await controller.delete_config(config_id)
